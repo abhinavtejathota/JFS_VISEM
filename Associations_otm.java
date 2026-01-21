09dc53195b53762338b9d73eed9c6c9a54697aea
Abhinav Teja Thota <162873293+abhinavtejathota@users.noreply.github.com>
2026-01-21 11:48:58 +0530

    OneToMany

---
+++ Associations_otm.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Employee {
+    int id;
+    String name;
+    float salary;
+    Employee (int id, String name, float salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+}
+
+class Department {
+    int deptId;
+    String deptName;
+    List<Employee> emplst;
+    Department (int deptId, String deptName, List<Employee> emplst){
+        this.deptId = deptId;
+        this.deptName = deptName;
+        this.emplst = emplst;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Employee e1 = new Employee(1, "Ravi", 10000);
+        Employee e2 = new Employee(2, "Anita", 15000);
+        List<Employee> empList = new ArrayList<Employee>();
+        empList.add(e1);
+        empList.add(e2);
+        Department dept = new Department(101, "CSE", empList);
+        System.out.println("Department:"+dept.deptName);
+        float total = 0;
+        for(Employee e : dept.emplst) {
+            System.out.println(e.id+" "+e.name+" "+e.salary);
+            total += e.salary;
+        }
+        System.out.println("avg salary: "+total/dept.emplst.size());
+    }
+}
